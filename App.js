36ec26a9eafd57fd4c8a22fa62ae4b3d623fc0f0
Shashankshekhar22 <itsshashankshekhar@gmail.com>
2024-05-03 20:14:15 +0530

    Changes for basic

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React element
+const jsxTutorial = <h1 id="heading"> Namaste React Tutorial using JSX 🚀</h1>;
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+const Title = () => {
+  return <h2>This is title</h2>;
+};
+const number = 10000;
+// React functional component
+const HeaderComponent = () => (
+  <div id="container">
+    {number}
+    <h1>Header from functional component</h1>
+    <Title />
+  </div>
 );
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(heading);
+
+const HeaderComponent2 = () => {
+  return <h1 className="something">Header Component</h1>;
+};
+
+// rootElement.render(heading);
+rootElement.render(<HeaderComponent />);
